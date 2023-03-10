4c9a1e408ad075be1935aecd5fc9e82056936333
Nikolay Dutskinov <niki.dutskinov@gmail.com>
2023-03-10 18:57:13 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dog-images-generator-ui
+//
+//  Created by Nikolay N. Dutskinov on 10.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
